fade21bbd74d936d3721b5d2753fb19d7c39925c
Garrett <g.romo2@students.clark.edu>
2020-06-08 22:44:02 -0700

    Completed Assignment

--- Prob-1/Prob-1.py
+++ Prob-1/Prob-1.py
@@ -2,4 +2,45 @@
 #   Programming Assignment 11
 #     Prob-1.py
 
-# Your code below
+class Car:
+    def __init__(self, make, model, year):
+        self._make = make
+        self._model = model
+        self._year = year
+    
+    def set_make(self, make):
+        self._make = make
+
+    def get_make(self):
+        return self._make
+
+    def set_model(self, model):
+        self._model = model
+
+    def get_model(self):
+        return self._model
+
+    def set_year(self, year):
+        self._year = year
+
+    def get_year(self):
+        return self._year
+
+def TestCar():
+    car1 = Car('Chevorlet', 'Sonora', '2006')
+    car2 = Car('Triumph', 'Tiger', '2004')
+    car3 = Car('Lexus', 'GS460', '2008')
+    car4 = Car('Kia', 'Borrego', '2009')
+    car5 = Car('Suzuki', 'GZ250', '2001')
+    carLot = [car1, car2, car3, car4, car5]
+    for car in carLot:
+        print("Make: ", car._make, "  ", "Model: ", car._model, "  ", "Year: ", car._year)
+    for car in carLot:
+        car.set_make(str(input("Enter a new make for the car: ")))
+        car.set_model(str(input("Enter a new model for the car: ")))
+        car.set_year(int(input("Enter a new year for the car: ")))
+    for car in carLot:
+        print("Make: ", car._make, "  ", "Model: ", car._model, "  ", "Year: ", car._year)
+        
+if __name__ == "__main__":
+    TestCar()
--- Prob-2/Prob-2.py
+++ Prob-2/Prob-2.py
@@ -2,4 +2,46 @@
 #   Programming Assignment 11
 #     Prob-2.py
 
-# Your code below
+class Student:
+    def __init__(self, name, ID, major, GPA):
+        self._name = name
+        self._ID = ID
+        self._major = major
+        self._GPA = GPA
+        # I added them all here, since I felt it would be best. I'm unsure what I'm doing, to be honest.
+    
+    def set_name(self, name):
+        self._name = name 
+
+    def get_name(self):
+        return self._name
+    
+    def set_ID(self, ID):
+        self._ID = ID
+
+    def get_ID(self):
+        return self._ID   
+
+    def set_major(self, major):
+        self._major = major
+
+    def get_major(self):
+        return self._major
+
+    def set_GPA(self, GPA):
+        self._GPA = GPA
+
+    def get_GPA(self):
+        return self._GPA 
+
+def studentTest():
+    JB = Student("Joe Bella", "9933", "Web Development", "3.8")
+    TB = Student("Tucker Blank", "3399", "Nursing", "3.0")
+    GU = Student("Gayle Ujifusa", "1011", "Baking", "2.8")
+    EA = Student("Edna Anker", "9875", "Medical Office", "3.0")
+    students = [JB, TB, GU, EA]
+
+    print("Name            ID Number       Major           GPA")
+    for student in students:
+        print('{:<15}'.format(student._name), '{:<15}'.format(student._ID), '{:<15}'.format(student._major), student._GPA)
+studentTest()
